7f2b13d790616617013e9dbf2c17246fd05f6874
ttrelvik <tom@trelvik.net>
2025-11-22 16:02:29 -0500

    feat: Export final RAG configuration

--- .docker/drupal/settings.local.php
+++ .docker/drupal/settings.local.php
@@ -39,7 +39,7 @@ if (getenv('DB_NAME')) {
 
 // --- Read Password from Docker Secret ---
 // Check if the secret file exists and override the password.
-$secret_path = '/run/secrets/drupal_cms-dev_postgres_password';
+$secret_path = '/run/secrets/drupal_cms-postgres_password';
 if (file_exists($secret_path)) {
   $databases['default']['default']['password'] = trim(file_get_contents($secret_path));
 }
